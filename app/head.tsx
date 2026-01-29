50d68068b31a842f91686743a759f2c3d9581622
deepanshu-kashyap <deepanshukashyap5252@gmail.com>
2026-01-30 01:45:52 +0530

    Fix favicon flash permanently

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="icon" href="/favicon.ico" sizes="any" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon" href="/apple-icon.png" />
+      <meta name="theme-color" content="#020617" />
+    </>
+  );
+}
